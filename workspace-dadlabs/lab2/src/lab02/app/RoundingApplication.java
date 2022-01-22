d0157df03ca5f5722649f038a20901ace5046785
faridhazeeq <farid.hazeeq@ymail.com>
2022-01-22 22:49:35 +0800

    Lab02 on week 3
    Alhamdulillah slowly understand to use github

---
+++ workspace-dadlabs/lab2/src/lab02/app/RoundingApplication.java
@@ -0,0 +1,10 @@
+package lab02.app;
+
+public class RoundingApplication {
+	public static void main(String[] args) {
+		
+		System.out.println("Rounding number to the nearest decimal points");
+	
+		}
+
+}
